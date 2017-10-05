4327be772ff84f3fa16d123fd8d1e80b2a6e668f
Sapariansyah Papank <rian070989@gmail.com>
2017-10-06 04:13:08 +0700

    Enable CORS.

--- public/index.php
+++ public/index.php
@@ -18,6 +18,19 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
+// Enable CORS
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
 
